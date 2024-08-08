795566ebe51b7a7a8e1b4242288847831f96f88f
Changwhi <whinada@gmail.com>
2024-08-07 23:04:18 -0700

    feat: import landing component to display landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
-import TestDB from "@/utils/db"
+import TestDB from "@/utils/db";
+import LandingPage from "@/components/landingPage";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-         <TestDB />
-         "Main"
-    </main>
+      <LandingPage />
   );
 }
