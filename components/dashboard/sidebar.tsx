8b3a86f662f7e09e3fd181ae8348a13fbb0d4456
Changwhi <whinada@gmail.com>
2024-08-14 00:43:52 -0700

    fix: adjust layout of menu

--- components/dashboard/sidebar.tsx
+++ components/dashboard/sidebar.tsx
@@ -2,70 +2,125 @@
 import { usePathname } from "next/navigation";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { CONSTANTS } from "@/text/sideBar";
-
 const Sidebar = ({ admin }: { admin: boolean }) => {
   const pathname = usePathname();
 
-  const getLinkClass = (link: string) => {
-    return pathname === link ? CONSTANTS.LINK_ACTIVE : CONSTANTS.LINK_INACTIVE;
-  };
+  // Helper function to determine if the menu item is active
+  const isActive = (path: string) => pathname.startsWith(path);
 
   return (
     <aside className="w-1/5 2xl:w-2/12 bg-black text-white p-6">
       <div className="py-10 flex flex-col items-start">
         <Avatar className="relative w-24 h-24">
-          <AvatarImage src="/avatar.png" alt={CONSTANTS.AVATAR_ALT} />
+          <AvatarImage src="/avatar.png" alt="User Avatar" />
           <AvatarFallback>CS</AvatarFallback>
         </Avatar>
-        <h2 className="mt-4 text-2xl font-bold">{CONSTANTS.USER_NAME}</h2>
-        <p className="mt-2 text-base">{CONSTANTS.USER_ROLE}</p>
+        <h2 className="mt-4 text-2xl font-bold">Chul Su</h2>
+        <p className="mt-2 text-base"></p>
         <a
           className="text-muted-foreground underline"
-          href={`mailto:${CONSTANTS.USER_EMAIL}`}
+          href="mailto:samantha@email.com"
           target="_blank"
         >
-          {CONSTANTS.USER_EMAIL}
+          samantha@email.com
         </a>
       </div>
-      <nav className="mt-8 space-y-8">
-        <a
-          href="/dashboard/admin/summary"
-          className={getLinkClass("/dashboard/admin/summary")}
-        >
-          {CONSTANTS.SUMMARY}
-        </a>
-        {admin && (
-          <>
-            <a
-              href="/dashboard/admin/attendance"
-              className={getLinkClass("/dashboard/admin/attendance")}
-            >
-              {CONSTANTS.ATTENDANCE}
-            </a>
-            <a
-              href="/dashboard/admin/members"
-              className={getLinkClass("/dashboard/admin/members")}
-            >
-              {CONSTANTS.MEMBERS}
-            </a>
-          </>
-        )}
-        <a
-          href="/dashboard/admin/events"
-          className={getLinkClass("/dashboard/admin/events")}
-        >
-          {CONSTANTS.EVENTS}
-        </a>
-        <a
-          href="/dashboard/admin/assignments"
-          className={getLinkClass("/dashboard/admin/assignments")}
-        >
-          {CONSTANTS.ASSIGNMENTS}
-        </a>
-        <a href="#" className={CONSTANTS.SETTINGS_DISABLED}>
-          {CONSTANTS.SETTINGS}
-        </a>
-      </nav>
+      {admin && (
+        <nav className="mt-8 space-y-8">
+          <a
+            href="/dashboard/summary"
+            className={`block text-lg font-bold ${
+              isActive("/dashboard/summary") ? "text-white" : "text-gray-500"
+            }`}
+          >
+            {CONSTANTS.SUMMARY}
+          </a>
+          <a
+            href="/dashboard/attendance"
+            className={`block text-lg font-bold ${
+              isActive("/dashboard/attendance") ? "text-white" : "text-gray-500"
+            }`}
+          >
+            {CONSTANTS.ATTENDANCE}
+          </a>
+          <a
+            href="/dashboard/events"
+            className={`block text-lg font-bold ${
+              isActive("/dashboard/events") ? "text-white" : "text-gray-500"
+            }`}
+          >
+            {CONSTANTS.EVENTS}
+          </a>
+          <a
+            href="/dashboard/assignments"
+            className={`block text-lg font-bold ${
+              isActive("/dashboard/assignments")
+                ? "text-white"
+                : "text-gray-500"
+            }`}
+          >
+            {CONSTANTS.ASSIGNMENTS}
+          </a>
+          <a
+            href="/dashboard/members"
+            className={`block text-lg font-bold ${
+              isActive("/dashboard/members") ? "text-white" : "text-gray-500"
+            }`}
+          >
+            {CONSTANTS.MEMBERS}
+          </a>
+          <a
+            href="/dashboard/settings"
+            className={`block text-lg text-muted-foreground ${
+              isActive("/dashboard/settings")
+                ? "text-white"
+                : "opacity-50 cursor-not-allowed"
+            }`}
+          >
+            {CONSTANTS.SETTINGS}
+          </a>
+        </nav>
+      )}
+      {!admin && (
+        <nav className="mt-8 space-y-8">
+          <a
+            href="/dashboard/summary"
+            className={`block text-lg font-bold ${
+              isActive("/dashboard/summary") ? "text-white" : "text-gray-500"
+            }`}
+          >
+            {CONSTANTS.SUMMARY}
+          </a>
+          <a
+            href="/dashboard/events"
+            className={`block text-lg font-bold ${
+              isActive("/dashboard/events") ? "text-white" : "text-gray-500"
+            }`}
+          >
+            {CONSTANTS.EVENTS}
+          </a>
+          <a
+            href="/dashboard/assignments"
+            className={`block text-lg font-bold ${
+              isActive("/dashboard/assignments")
+                ? "text-white"
+                : "text-gray-500"
+            }`}
+          >
+            {CONSTANTS.ASSIGNMENTS}
+          </a>
+          <a
+            href="/dashboard/settings"
+            className={`block text-lg text-muted-foreground ${
+              isActive("/dashboard/settings")
+                ? "text-white"
+                : "opacity-50 cursor-not-allowed"
+            }`}
+          >
+            {CONSTANTS.SETTINGS}
+          </a>
+        </nav>
+      )}
     </aside>
   );
 };
