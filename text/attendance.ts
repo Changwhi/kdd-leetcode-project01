32eddfddfa6614d7c202e2e47da38d81235589ac
Changwhi <whinada@gmail.com>
2024-08-15 00:42:52 -0700

    feat: store all text in a attendance component

---
+++ text/attendance.ts
@@ -0,0 +1,10 @@
+export const CONSTANTS = {
+  SELECT_EVENTS: "Select events",
+  SEE_PROJECT_DETAILS: "See project details",
+  NAME: "Name",
+  SELF_CHECK_IN: "Self check-in",
+  INDIVIDUAL_QUESTION: "Individual question",
+  PULL_REQUEST: "Pull request",
+  CHECK: "Check",
+};
+
