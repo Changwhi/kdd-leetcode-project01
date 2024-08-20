fd4ab9db1900085bdd4e7704a57634ff97acd3da
Changwhi <whinada@gmail.com>
2024-08-19 21:00:28 -0700

    feat: implement event card component to wrap event info

---
+++ components/admin/events/event-card.tsx
@@ -0,0 +1,44 @@
+// components/admin/events/EventCard.tsx
+import React from 'react';
+import { Button } from '@/components/ui/button';
+
+interface EventCardProps {
+  title: string;
+  date: string;
+  description: string;
+  link: string;
+}
+
+export const EventCard: React.FC<EventCardProps> = ({ title, date, description, link }) => {
+
+  const handleDetails = () => {
+     
+  }
+
+  return (
+    <div className="flex flex-col rounded-lg overflow-hidden shadow-lg min-w-full max-w-xs mx-auto hover:shadow-xl transition-all duration-200">
+      <div className="flex flex-col p-4 flex-grow">
+        <div className="flex flex-col flex-grow">
+          <h2 className="text-xl font-semibold hover:text-gray-700 duration-200">{title}</h2>
+          <h3 className="text-gray-500 hover:text-gray-600 transition-all duration-200">{date}</h3>
+          <p className="mt-2 text-gray-600 hover:text-gray-700 transition-all duration-200 flex-grow">
+            {description}
+          </p>
+        </div>
+        <div className="flex flex-col items-center mt-4 space-x-2">
+          <Button onClick={() => window.open(link, '_blank')} className="w-full hover:bg-gray-700 hover:text-white transition-all duration-200" size="sm">
+            Detail
+          </Button>
+          <a
+            className="text-muted-foreground underline"
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {link}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
