f728c710b8641c0a8cbde26488db8d51352dc67e
Changwhi <whinada@gmail.com>
2024-08-22 00:40:46 -0700

    feat: import addEvent function and use action property in form element

--- components/admin/events/create-event-modal.tsx
+++ components/admin/events/create-event-modal.tsx
@@ -1,52 +1,45 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Modal } from "./modal"; 
+import { Modal } from "./modal";
+import { addEvent } from "@/lib/actions/event";
 
-export function EventModal({ isOpen, submitFunction, onClose}: { isOpen: boolean; submitFunction: ({title, date, description, link} : any) => void; onClose: () => void }) {
-  const [title, setTitle] = useState("");
-  const [date, setDate] = useState("");
-  const [description, setDescription] = useState("");
-  const [link, setLink] = useState("");
-
-  const handleSubmit = () => {
-    submitFunction({ title, date, description, link });
-    setTitle("");
-    setDate("");
-    setDescription("");
-    setLink("");
-
-  };
-
+export function EventModal({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean;
+  submitFunction: ({ title, date, description, link }: any) => void;
+  onClose: () => void;
+}) {
+  const ref = useRef<HTMLFormElement>(null);
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <h2 className="text-lg font-bold">Create Event</h2>
-      <form className="space-y-4">
-        <Input
-          placeholder="Event Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <Input
-          type="date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-        <Input
-          placeholder="Event Description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <Input
-          placeholder="Event Link"
-          value={link}
-          onChange={(e) => setLink(e.target.value)}
-        />
+      <form
+        ref={ref}
+        className="space-y-4"
+        action={async (formData) => {
+          ref.current?.reset();
+          await addEvent({
+            name: formData.get("name") as string,
+            date: formData.get("date") as string,
+            topic: formData.get("topic") as string,
+            zoomlink: formData.get("zoomlink") as string,
+            group_id: 1 as number,
+            event_id: 0,
+          });
+        }}
+      >
+        <Input placeholder="Event Title" name="name" />
+        <Input type="date" name="date" />
+        <Input placeholder="Event Description" name="topic" />
+        <Input placeholder="Event Link" name="zoomlink" />
         <div className="flex justify-end space-x-4">
           <Button variant="outline" onClick={onClose}>
             Cancel
           </Button>
-          <Button variant="default" onClick={handleSubmit}>
+          <Button variant="default" type="submit">
             Create
           </Button>
         </div>
