5c889038dc58d5bb383ffc70e1531350eec4ae84
Changwhi <whinada@gmail.com>
2024-08-23 11:46:17 -0700

    design: redsign event card for better UX

--- components/admin/events/event-card.tsx
+++ components/admin/events/event-card.tsx
@@ -19,45 +19,39 @@ export const EventCard: React.FC<EventCardProps> = ({
   topic,
   zoomlink,
 }) => {
-
   return (
     <div className="flex flex-col rounded-lg overflow-hidden shadow-lg min-w-full max-w-xs mx-auto hover:shadow-xl transition-all duration-200">
       <div className="flex flex-col p-4 flex-grow">
         <div className="flex flex-col flex-grow p-4">
-          <hr className="w-full border-gray-300" />
-          <h2 className="h-20 overflow-y-hidden text-xl font-semibold hover:text-gray-700 duration-200">
+          <h2 className="h-6 overflow-y-hidden text-base font-semibold hover:text-gray-700 duration-200">
             {name}
           </h2>
           <hr className="w-full border-gray-300" />
-          <h3 className="text-xs text-gray-500 hover:text-gray-600 transition-all duration-200">
+          <h3 className="text-xs text-gray-400 hover:text-gray-600 transition-all duration-200">
             {date.toString()}
           </h3>
-          <hr className="w-full border-gray-300" />
-          <p className="h-16 mt-2 text-gray-600 hover:text-gray-700 transition-all duration-200 flex-grow">
+          <p className="h-16 text-sm mt-2 text-gray-600 hover:text-gray-700 transition-all duration-200 flex-grow">
             {topic}
           </p>
-          <div>
+        </div>
+        <div className="flex flex-raw justify-between items-center mt-2 space-x-2">
             <a
-              className="text-muted-foreground underline"
+              className="text-sm text-muted-foreground underline"
               href={zoomlink}
               target="_blank"
               rel="noopener noreferrer"
             >
               {EVENTS.LINK}
             </a>
-          </div>
-        </div>
-        <div className="flex flex-col items-center mt-2 space-x-2">
           <Dialog>
             <DialogTrigger asChild>
-              <Button variant="outline">Edit Profile</Button>
+              <Button variant="outline">Edit</Button>
             </DialogTrigger>
             <DialogContent className="sm:max-w-[425px]">
               <DialogHeader>
-                <DialogTitle>Edit profile</DialogTitle>
+                <DialogTitle>Edit le</DialogTitle>
                 <DialogDescription>
-                  Make changes to your profile here. Click save whenre
-                  done.
+                  Make changes to your profile here. Click save whenre done.
                 </DialogDescription>
               </DialogHeader>
               <div className="grid gap-4 py-4">
