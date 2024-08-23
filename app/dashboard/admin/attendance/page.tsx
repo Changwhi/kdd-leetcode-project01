7a202a60334bdd20c4ae120a63233ace98df36fb
Changwhi <whinada@gmail.com>
2024-08-23 12:28:53 -0700

    feat: retrieve event list and display them dinamically

--- app/dashboard/admin/attendance/page.tsx
+++ app/dashboard/admin/attendance/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 import { Card } from "@/components/ui/card";
-import Link from "next/link";
 import { CONSTANTS } from "@/text/attendance";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AttendanceTable } from "@/components/attendance/attendanceTable";
 import { ParticipantsIcon } from "@/components/attendance/icons/participantsIcon";
 import { AttendedIcon } from "@/components/attendance/icons/attendedIcon";
 import { AbsentIcon } from "@/components/attendance/icons/absentIcon";
+import { useToast } from "@/components/ui/use-toast";
+import { retrieveEvents } from "@/lib/actions/event";
+import { EventType } from "@/types/event";
+import moment from "moment";
 
 const eventsData = [
   {
@@ -39,19 +42,35 @@ const eventsData = [
 ];
 
 export default function Attendance() {
-  const [selectedEvent, setSelectedEvent] = useState(eventsData[0]);
+  const { toast } = useToast();
+  const [selectedEvent, setSelectedEvent] = useState<EventType>();
+  const [events, setEvents] = useState<EventType[]>([]);
 
-  const handleEvent = (event: (typeof eventsData)[0]) => {
+  const handleEvent = (event: EventType) => {
     setSelectedEvent(event);
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const eventList = await retrieveEvents();
+        setEvents(eventList);
+        setSelectedEvent(eventList[0]);
+      } catch (error) {
+        toast({ title: "Error", description: "Fail to load data" });
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="flex flex-col min-h-full lg:flex-row">
       <main className="basis-3/4 p-6 bg-white lg:pr-6 lg:w-3/4">
         <div className="flex justify-between items-center mb-6">
           <div>
-            <h1 className="text-xl font-bold">{selectedEvent.title}</h1>
-            <p className="text-muted-foreground">{selectedEvent.date}</p>
+            <h1 className="text-xl font-bold">{selectedEvent?.name}</h1>
+            <p className="text-muted-foreground">{moment(selectedEvent?.date).format("MMMM Do YYYY, h:mm a")}</p>
             <div className="flex gap-4 p-5">
               <Card className="w-40 p-4 ">
                 <div className="flex gap-2 items-center">
@@ -61,7 +80,7 @@ export default function Attendance() {
                   </h3>
                 </div>
                 <p className="pt-3 text-center text-base font-semibold">
-                  {selectedEvent.totalParticipants} people
+                  {5} people
                 </p>
               </Card>
               <Card className="w-40 p-4 ">
@@ -70,7 +89,7 @@ export default function Attendance() {
                   <h3 className="text-xs text-muted-foreground">Attended</h3>
                 </div>
                 <p className="pt-3 text-center text-base font-semibold">
-                  {selectedEvent.totalPresent} people
+                  {3} people
                 </p>
               </Card>
               <Card className="w-40 p-4 ">
@@ -79,34 +98,28 @@ export default function Attendance() {
                   <h3 className="text-xs text-muted-foreground">Absent</h3>
                 </div>
                 <p className="pt-3 text-left text-base font-semibold">
-                  {selectedEvent.totalAbsent} people
+                  {2} people
                 </p>
               </Card>
             </div>
           </div>
         </div>
-        <div className="overflow-x-auto">{selectedEvent.description}</div>
+        <div className="overflow-x-auto">{selectedEvent?.topic}</div>
         <AttendanceTable />
       </main>
       <aside className="basis-1/4 xl:w-80 bg-slate-50 p-6 rounded-xl">
         <div className="mb-6">
           <div className="space-y-4 mt-4">
             <h2 className="text-lg font-bold">{CONSTANTS.SELECT_EVENTS}</h2>
-            {eventsData.map((event, index) => (
+            {events.map((event, index) => (
               <Card
                 key={index}
                 className="p-4 hover:bg-slate-300 hover:cursor-pointer"
                 onClick={() => handleEvent(event)}
               >
-                <h3 className="text-sm font-semibold">{event.title}</h3>
-                <p className="text-xs text-muted-foreground">{event.date}</p>
-                <Link
-                  href={event.link}
-                  className="text-blue-500 text-sm"
-                  prefetch={false}
-                >
-                  {CONSTANTS.SEE_PROJECT_DETAILS}
-                </Link>
+                <h3 className="text-lg font-semibold">{event.name}</h3>
+                <p className="text-base">{event.topic}</p>
+                <p className="text-xs text-gray-400 text-muted-foreground">{moment(event.date).format("MMMM Do YYYY, h:mm a")}</p>
               </Card>
             ))}
           </div>
