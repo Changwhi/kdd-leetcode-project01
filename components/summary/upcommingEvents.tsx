c6861f2e2f45fb1dcb3bed03fda6c18856bfba09
Changwhi <whinada@gmail.com>
2024-08-23 16:49:28 -0700

    feat: replace mock data with actual data from DB

--- components/summary/upcommingEvents.tsx
+++ components/summary/upcommingEvents.tsx
@@ -1,38 +1,47 @@
-import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import { Calendar } from "@/components/ui/calendar";
 import Link from "next/link";
 import { CONSTANTS } from "@/text/summary";
+import moment from "moment";
+import { retrieveEvents } from "@/lib/actions/event";
 import { useEffect, useState } from "react";
-import { SummaryTable } from "./summaryTable";
-import { Assignments } from "./assignments";
+import { EventType } from "@/types/event";
 
-const eventsData = [
-  {
-    title: "Week 1 – Online Mock Interview",
-    date: "April 04, 2024",
-    link: "#",
-  },
-  {
-    title: "Week 2 – Online Mock Interview",
-    date: "April 04, 2024",
-    link: "#",
-  },
-  {
-    title: "Week 3 – Online Mock Interview",
-    date: "April 04, 2024",
-    link: "#",
-  },
-];
 export const UpcommingEvents = () => {
+  const currentDate = moment();
+  const [eventsData, setEventsData] = useState<EventType[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await retrieveEvents();
+        if (!response) {
+          return;
+        }
+        const upcommingEvents = response.filter((event) =>
+          moment(event.date).isAfter(currentDate)
+        );
+        setEventsData(upcommingEvents);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="space-y-4 mt-4">
       <h2 className="text-lg font-bold">{CONSTANTS.UPCOMING_EVENTS_TITLE}</h2>
       {eventsData.map((event, index) => (
         <Card key={index} className="p-4">
-          <h3 className="text-sm font-semibold">{event.title}</h3>
-          <p className="text-muted-foreground">{event.date}</p>
-          <Link href={event.link} className="text-blue-500" prefetch={false}>
+          <h3 className="text-sm font-semibold">{event.name}</h3>
+          <p className="text-muted-foreground">
+            {moment(event.date).format("MMMM Do YYYY, h:mm a")}
+          </p>
+          <Link
+            href={event.zoomlink}
+            className="text-blue-500"
+            prefetch={false}
+          >
             {CONSTANTS.SEE_PROJECT_DETAILS}
           </Link>
         </Card>
