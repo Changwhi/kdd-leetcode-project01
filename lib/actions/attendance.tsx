0d851dabb50d65b867ca38920ccf39a48e89f0ab
Changwhi <whinada@gmail.com>
2024-08-23 15:29:22 -0700

    feat: update query to check pr state

--- lib/actions/attendance.tsx
+++ lib/actions/attendance.tsx
@@ -9,12 +9,13 @@ export const retrieveAttendance = async ({
 }) => {
   try {
     const response = await sql`
-SELECT user_group_id, user_type, init_amount, curr_amout, user_group.user_id, "user".name, "user".email, attendance.attended,  submission_id
+SELECT user_group_id, user_type, init_amount, curr_amout, user_group.user_id, "user".name, "user".email, attendance.attended,  submission_id, pr.submitted
 FROM user_group
 LEFT JOIN "user" ON "user".user_id = user_group.user_id
 left join attendance on "user".user_id = attendance.user_id and attendance.event_id = ${event_id}
 left join "event" on "event".event_id = attendance.event_id
 left join submission on submission.user_id = "user".user_id and submission.event_id = "event".event_id
+left join pr on pr.user_id = "user".user_id and pr.event_id = "event".event_id
 WHERE user_group.group_id = 1;
     `;
 
