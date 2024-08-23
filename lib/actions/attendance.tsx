dd716458aceb1bed06de5f56f48bc726fef504a9
Changwhi <whinada@gmail.com>
2024-08-23 15:17:14 -0700

    feat: update query to organaize columns

--- lib/actions/attendance.tsx
+++ lib/actions/attendance.tsx
@@ -9,11 +9,12 @@ export const retrieveAttendance = async ({
 }) => {
   try {
     const response = await sql`
-SELECT * 
+SELECT user_group_id, user_type, init_amount, curr_amout, user_group.user_id, "user".name, "user".email, attendance.attended,  submission_id
 FROM user_group
 LEFT JOIN "user" ON "user".user_id = user_group.user_id
 left join attendance on "user".user_id = attendance.user_id and attendance.event_id = ${event_id}
-left join submission on submission.user_id = "user".user_id
+left join "event" on "event".event_id = attendance.event_id
+left join submission on submission.user_id = "user".user_id and submission.event_id = "event".event_id
 WHERE user_group.group_id = 1;
     `;
 
