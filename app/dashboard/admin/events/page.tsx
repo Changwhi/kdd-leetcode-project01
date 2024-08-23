40f23ea5f041d10b0e9a7439258fcebceefcb667
Changwhi <whinada@gmail.com>
2024-08-23 00:15:48 -0700

    design: Remove unnecessary second unit in event card

--- app/dashboard/admin/events/page.tsx
+++ app/dashboard/admin/events/page.tsx
@@ -38,7 +38,7 @@ export default function Events() {
     <div className="flex flex-col min-h-full lg:flex-row">
       <main className="basis-3/4 p-6 bg-white lg:pr-6 lg:w-3/4">
         <div className="flex flex-col gap-4 justify-start items-start">
-          <h2 className="text-2xl font-bold">{EVENTS.UPCOMMINGEVENTS}</h2>
+          <h2 className="text-2xl font-bold">{EVENTS.UPCOMINGEVENTS}</h2>
           <hr className="w-full border-gray-300" />
           <div className="grid grid-cols-2 xl:grid-cols-3 mb-6 gap-5 ">
             {events.map((event, index) => (
@@ -46,7 +46,7 @@ export default function Events() {
                 key={event.event_id}
                 event_id={event.event_id}
                 name={event.name}
-                date={moment(event.date).format("MMMM Do YYYY, h:mm:ss a")}
+                date={moment(event.date).format("MMMM Do YYYY, h:mm a")}
                 topic={event.topic}
                 zoomlink={event.zoomlink}
                 group_id={1}
