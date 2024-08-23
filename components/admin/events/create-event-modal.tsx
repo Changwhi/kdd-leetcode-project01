b60518fc3964910af7831c95f4dc3056a21082cc
Changwhi <whinada@gmail.com>
2024-08-23 11:06:03 -0700

    style: remove redudant code and import constant text

--- components/admin/events/create-event-modal.tsx
+++ components/admin/events/create-event-modal.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { revalidatePath } from "next/cache";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
@@ -11,7 +11,6 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { addEvent } from "@/lib/actions/event";
 import { EVENTS } from "@/text/events";
 
@@ -23,14 +22,14 @@ export const CreateEventModal = () => {
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
+          <DialogTitle>{EVENTS.DIALOG_TITLE}</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+          {EVENTS.DIALOG_DESCRIPTION}
           </DialogDescription>
         </DialogHeader>
         <form
           className="space-y-4"
-          action={async (formData) => {
+          action={async (formData: FormData) => {
             await addEvent({
               name: formData.get("name") as string,
               date: formData.get("date") as string,
@@ -39,30 +38,19 @@ export const CreateEventModal = () => {
               group_id: 1 as number,
               event_id: 0,
             });
-            revalidatePath("/dashboard/admin/events");
           }}
         >
           <div className="flex flex-col justfy-start items-center gap-4 py-4">
-            {/* <Label htmlFor="name" className="text-right">
-                      Event Title:
-                    </Label> */}
             <Input placeholder="Event Title" name="name" />
-            {/* <Label htmlFor="username" className="text-right">
-                      Date:
-                    </Label> */}
             <Input type="date" name="date" />
-            {/* <Label htmlFor="Event Description" className="text-right">
-                      Event Description:
-                    </Label> */}
             <Input placeholder="Event Description" name="topic" />
-            {/* <Label htmlFor="Event Link" className="text-right">
-                      Event Link:
-                    </Label> */}
             <Input placeholder="Event Link" name="zoomlink" />
           </div>
-          <DialogFooter>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
+          <DialogClose asChild>
+            <DialogFooter>
+              <Button type="submit">{EVENTS.CREATE}</Button>
+            </DialogFooter>
+          </DialogClose>
         </form>
       </DialogContent>
     </Dialog>
