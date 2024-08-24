af9aabcafd24a28e106eac2aefc38f3da351076a
Changwhi <whinada@gmail.com>
2024-08-24 00:31:44 -0700

    refactor: convert client to server component for clarity and performance

--- components/summary/upcommingEvents.tsx
+++ components/summary/upcommingEvents.tsx
@@ -1,37 +1,15 @@
+"use server"
 import { Card } from "@/components/ui/card";
 import Link from "next/link";
-import { CONSTANTS } from "@/text/summary";
+import { SUMMARY } from "@/text/summary";
 import moment from "moment";
-import { retrieveEvents } from "@/lib/actions/event";
-import { useEffect, useState } from "react";
 import { EventType } from "@/types/event";
 
-export const UpcommingEvents = () => {
-  const currentDate = moment();
-  const [eventsData, setEventsData] = useState<EventType[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await retrieveEvents();
-        if (!response) {
-          return;
-        }
-        const upcommingEvents = response.filter((event) =>
-          moment(event.date).isAfter(currentDate)
-        );
-        setEventsData(upcommingEvents);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, []);
-
+export const UpcommingEvents = ({events}: {events: EventType[]}) => {
   return (
     <div className="space-y-4 mt-4">
-      <h2 className="text-lg font-bold">{CONSTANTS.UPCOMING_EVENTS_TITLE}</h2>
-      {eventsData.map((event, index) => (
+      <h2 className="text-lg font-bold">{SUMMARY.UPCOMING_EVENTS_TITLE}</h2>
+      {events.length > 0 && events.map((event, index) => (
         <Card key={index} className="p-4">
           <h3 className="text-sm font-semibold">{event.name}</h3>
           <p className="text-muted-foreground">
@@ -42,10 +20,15 @@ export const UpcommingEvents = () => {
             className="text-blue-500"
             prefetch={false}
           >
-            {CONSTANTS.SEE_PROJECT_DETAILS}
+            {SUMMARY.SEE_PROJECT_DETAILS}
           </Link>
         </Card>
       ))}
+      {events.length === 0 && (
+        <p className="text-muted-foreground">
+          {SUMMARY.NO_UPCOMING_EVENTS}
+        </p>
+      )}
     </div>
   );
 };
