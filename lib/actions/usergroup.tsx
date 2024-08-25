9a91e0567ef04b1446b341fd2234e9a79c0cb97b
Soohyeun <soohyeunp@gmail.com>
2024-08-25 01:27:13 -0700

    Updated user_group database functions

--- lib/actions/usergroup.tsx
+++ lib/actions/usergroup.tsx
@@ -7,12 +7,36 @@ import { ResponseType } from "@/types/response";
  * Returns all users in a given group
  *
  * @param group_id - Group ID as a number
- * @returns An array of users if users exist, otherwise empty array
+ * @returns An array of user_group if exist, otherwise an empty array
  */
-export const retrieveAllUsersByGroup = async (group_id: number) => {
+export const retrieveAllUsersByGroup = async (
+  group_id: number
+): Promise<UserGroupType[]> => {
   try {
     const response: UserGroupType[] =
       await sql`SELECT * FROM user_group WHERE group_id=${group_id}`;
+    if (response.length > 0) {
+      return response;
+    }
+    return [];
+  } catch (error) {
+    console.error("Error retrieving users:", error);
+    return [];
+  }
+};
+
+/**
+ * Returns all groups along with a given user
+ *
+ * @param user_id - user_id as a number
+ * @returns An array of user_group if exist, otherwise an empty array
+ */
+export const retrieveAllGroupsByUsers = async (
+  user_id: number
+): Promise<UserGroupType[]> => {
+  try {
+    const response: UserGroupType[] =
+      await sql`SELECT * FROM user_group WHERE user_id=${user_id}`;
     if (response.length > 0) {
       return response;
     }
@@ -28,19 +52,22 @@ export const retrieveAllUsersByGroup = async (group_id: number) => {
  *
  * @param user_id - A user ID as a number
  * @param group_id - A group ID as a number
- * @returns A user object if user exists, otherwise null
+ * @returns An array of user_group if exist, otherwise an empty array
  */
-export const retrieveUserType = async (user_id: number, group_id: number) => {
+export const retrieveUserGroup = async (
+  user_id: number,
+  group_id: number
+): Promise<UserGroupType[]> => {
   try {
     const response: UserGroupType[] =
       await sql`SELECT user_type FROM user_group WHERE group_id=${group_id} AND user_id=${user_id}`;
     if (response.length > 0) {
-      return response[0];
+      return response;
     }
-    return null;
+    return [];
   } catch (error) {
     console.error("Error retrieving user:", error);
-    return null;
+    return [];
   }
 };
 
@@ -48,12 +75,14 @@ export const retrieveUserType = async (user_id: number, group_id: number) => {
  * Create new user_group column when user joins a new group
  *
  * @param formData - A user_group data as a UserGroupProps object
- * @returns A response object
+ * @returns a success message or an error message
  */
-export const createUserGroup = async (formData: UserGroupProps) => {
+export const createUserGroup = async (
+  formData: UserGroupProps
+): Promise<string> => {
   try {
     if (!formData) {
-      return [];
+      return "Data form is not exist";
     }
     console.log(formData);
     const user_type = formData.user_type;
@@ -62,13 +91,20 @@ export const createUserGroup = async (formData: UserGroupProps) => {
     const group_id = formData.group_id;
     const user_id = formData.user_id;
 
+    // Prevent make duplicate
+    const existingUserGroup = await retrieveUserGroup(user_id, group_id);
+    if (existingUserGroup.length > 0) {
+      return "User is already in a group.";
+    }
+
     const response: ResponseType[] = await sql`
       INSERT INTO user_group (user_type, init_amount, curr_amount, group_id, user_id)
       VALUES (${user_type}, ${init_amount}, ${curr_amount}, ${group_id}, ${user_id})
     `;
-    return response;
+    return "User_group created successfully.";
   } catch (error) {
     console.log(error);
+    return "Failed to create user_group.";
   }
 };
 
@@ -77,15 +113,19 @@ export const createUserGroup = async (formData: UserGroupProps) => {
  *
  * @param user_id - A user ID as a number
  * @param group_id - A group ID as a number
- * @returns A response object
+ * @returns a success message or an error message
  */
-export const deleteUserGroup = async (user_id: number, group_id: number) => {
+export const deleteUserGroup = async (
+  user_id: number,
+  group_id: number
+): Promise<string> => {
   try {
     const response: ResponseType[] = await sql`
     DELETE FROM "user" WHERE user_id = ${user_id} AND group_id = ${group_id}
     `;
-    return response;
+    return "User_group deleted successfully.";
   } catch (error) {
     console.log(error);
+    return "Failed to delete user_group.";
   }
 };
