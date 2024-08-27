075bb57d50f4bd101f47bd368aa3ca77a749713f
Soohyeun <soohyeunp@gmail.com>
2024-08-26 23:29:44 -0700

    Extract Icons to separate files

---
+++ components/attendance/icons/exclamationIcon.tsx
@@ -0,0 +1,21 @@
+export const ExclamationIcon = (props: any) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <circle cx="12" cy="12" r="12" fill="#FFC83D" />
+      <path
+        d="M12 7V13"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <circle cx="12" cy="17" r="1" fill="white" />
+    </svg>
+  );
+};
