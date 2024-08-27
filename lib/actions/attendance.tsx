a283b1396fa91ad161900713025af4195cbaf227
Changwhi <whinada@gmail.com>
2024-08-26 19:10:34 -0700

    feat: add group_id prop with fallback value which is 1

--- lib/actions/attendance.tsx
+++ lib/actions/attendance.tsx
@@ -9,8 +9,10 @@ import { sql } from "@/utils/db";
  */
 export const retrieveAttendance = async ({
   event_id,
+  group_id = 1,
 }: {
   event_id: number;
+  group_id?: number;
 }) => {
   try {
     const response: AttendanceType[] = await sql`
@@ -21,7 +23,7 @@ left join attendance on "user".user_id = attendance.user_id and attendance.event
 left join "event" on "event".event_id = attendance.event_id
 left join submission on submission.user_id = "user".user_id and submission.event_id = "event".event_id
 left join pr on pr.user_id = "user".user_id and pr.event_id = "event".event_id
-WHERE user_group.group_id = 1;
+WHERE user_group.group_id = ${group_id};
     `;
 
     if (response) {
