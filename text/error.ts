72cafd4c4a5579e71d096939372d319416ec47db
Changwhi <whinada@gmail.com>
2024-08-26 22:52:19 -0700

    feat: add new text

--- text/error.ts
+++ text/error.ts
@@ -2,4 +2,10 @@ export const ERROR = {
     GENERIC: "Something went wrong. Please try again later.",
     INVALID_CREDENTIALS: "Invalid credentials. Please try again.",
     PAGE_NOT_FOUND: "Page not found. Please try again later.",
+    FAIL_TO_CHECK_IN: "Failed to check in. Please try again later.",
+    CHECK_IN_SUCCESS: "Check-in successful.",
+    ERROR: "Error",
+    SUCCESS: "Success",
+
+
 }
