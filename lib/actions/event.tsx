c775a0a08cc04c5e0a313219333385d126a0946d
Changwhi <whinada@gmail.com>
2024-08-27 10:47:47 -0700

    feat: implement function to update event

--- lib/actions/event.tsx
+++ lib/actions/event.tsx
@@ -1,6 +1,6 @@
 "use server";
 import { sql } from "@/utils/db";
-import { EventCardProps, EventType } from "@/types/event";
+import { EventCardPropsForDB, EventType } from "@/types/event";
 import { revalidatePath } from "next/cache";
 /**
  * Retrieves a list of events from the database.
@@ -26,7 +26,7 @@ export const retrieveEvents = async () => {
  * @param {EventCardProps} formData - The data for the new event.
  * @return {Promise<void>} A promise that resolves when the event is successfully added, or rejects with an error if there is an issue.
  */
-export const addEvent = async (formData: EventCardProps) => {
+export const addEvent = async (formData: EventCardPropsForDB) => {
   try {
     if (!formData) {
       return [];
@@ -37,13 +37,60 @@ export const addEvent = async (formData: EventCardProps) => {
     const topic = formData.topic;
     const zoomlink = formData.zoomlink;
     const group_id = formData.group_id;
+    const assign1 = formData.assign1;
+    const assign2 = formData.assign2;
+    const assign3 = formData.assign3;
 
     await sql`
-      INSERT INTO event (name, date, topic, zoomlink, group_id)
-      VALUES (${name}, ${date}, ${topic}, ${zoomlink}, ${group_id})
+      INSERT INTO event (name, date, topic, zoomlink, group_id, assign1, assign2, assign3)
+      VALUES (${name}, ${date}, ${topic}, ${zoomlink}, ${group_id}, ${assign1}, ${assign2}, ${assign3})
     `;
     revalidatePath("/dashboard/admin/events");
   } catch (error) {
     console.log(error);
   }
 };
+
+/**
+ * Updates an existing event in the database.
+ *
+ * @param {EventCardProps} formData - The new data for the event.
+ * @return {Promise<void>} A promise that resolves when the event is successfully updated, or rejects with an error if there is an issue.
+ */
+export const updateEvent = async (formData: EventCardPropsForDB) => {
+  try {
+    if (!formData) {
+      return [];
+    }
+    const name = formData.name;
+    const date = formData.date;
+    const topic = formData.topic;
+    const zoomlink = formData.zoomlink;
+    const group_id = formData.group_id;
+    const assign1 = formData.assign1;
+    const assign2 = formData.assign2;
+    const assign3 = formData.assign3;
+    const event_id = formData.event_id;
+    await sql`
+      UPDATE event
+      SET name = ${name}, date = ${date}, topic = ${topic}, zoomlink = ${zoomlink}, group_id = ${group_id}, assign1 = ${assign1}, assign2 = ${assign2}, assign3 = ${assign3}
+      WHERE event_id = ${event_id}
+    `;
+    revalidatePath("/dashboard/admin/events");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// export const deleteEvent = async (event_id: number) => {
+//   try {
+//     await sql`
+//       DELETE FROM event
+//       WHERE event_id = ${event_id}
+//     `;
+//     revalidatePath("/dashboard/admin/events");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
