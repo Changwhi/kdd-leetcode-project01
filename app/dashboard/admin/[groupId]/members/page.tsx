53b3b73a94f375f72305c968e92bacaba6acf4cb
Changwhi <whinada@gmail.com>
2024-09-18 16:03:37 -0700

    feat: create and import a members component

--- app/dashboard/admin/[groupId]/members/page.tsx
+++ app/dashboard/admin/[groupId]/members/page.tsx
@@ -1,10 +1,5 @@
-export default function Members() {
-  return (
-    <div className="flex flex-col min-h-full lg:flex-row">
-      <main className="basis-3/4 p-6 bg-white lg:pr-6 lg:w-3/4">
-        <h2 className="text-2xl font-bold">Members</h2>
-        <div></div>
-      </main>
-    </div>
-  );
+import Members from "@/components/admin/members/";
+
+export default function MembersPage({params}: {params: {groupId: string}}) {
+  return <Members group_id={Number(params.groupId)}/>;
 }
