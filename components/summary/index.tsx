fa4e673a5d54f49cf45dc870d96c9fe817e6ebc1
Changwhi <whinada@gmail.com>
2024-09-20 00:16:29 -0700

    design: re-arrange component for better UI

--- components/summary/index.tsx
+++ components/summary/index.tsx
@@ -1,7 +1,6 @@
 "use server";
 import { Button } from "@/components/ui/button";
 import { SUMMARY } from "@/text/summary";
-import { SummaryTable } from "./summaryTable";
 import { UpcommingEvents } from "./upcommingEvents";
 import moment from "moment";
 import { retrieveEvents } from "@/lib/actions/event";
@@ -9,10 +8,17 @@ import { retrieveAllUsers } from "@/lib/actions/summary";
 import { SelectedEvent } from "./selectedEvent";
 import { CalendarTool } from "./calendar";
 import { EventProvider } from "@/lib/context/selectedEventContext";
+import SummaryTable from "./summaryTable";
 
-export const Summary = async ({ group_id, admin }: { group_id: number, admin:boolean}) => {
+export const Summary = async ({
+  group_id,
+  admin,
+}: {
+  group_id: number;
+  admin: boolean;
+}) => {
   const users = await retrieveAllUsers({ group_id });
-  const events = await retrieveEvents( group_id );
+  const events = await retrieveEvents(group_id);
   const eventdates = events.map((event) => new Date(event.date));
   const upcomingEvents = events.filter((event) =>
     moment(event.date).isAfter(moment())
@@ -22,35 +28,29 @@ export const Summary = async ({ group_id, admin }: { group_id: number, admin:boo
     <EventProvider>
       <div className="flex flex-col min-h-full lg:flex-row">
         <main className="basis-3/4 p-6 bg-white lg:pr-6 lg:w-3/4">
-          <div className="flex justify-between items-center mb-6">
+          <div className="flex justify-between items-center">
             <div>
               <h1 className="text-xl font-bold">{SUMMARY.TITLE}</h1>
             </div>
           </div>
-          <div className="overflow-x-auto">
-            <h1 className="text-base font-semibold">{SUMMARY.TABLE_TITLE}</h1>
-            <SummaryTable users={users} />
+          <div className="mb-6">
+            <UpcommingEvents events={upcomingEvents} />
           </div>
-          <div className="mt-6">
-            <h1 className="text-base font-semibold">{SUMMARY.EVENT_TITLE}</h1>
-            <SelectedEvent givenEvents={events} group_id={group_id} admin={admin}/>
+          <div className="overflow-x-auto mt-5">
+            <h1 className="text-lg font-bold">{SUMMARY.TABLE_TITLE}</h1>
+            <SummaryTable usersInGroup={users} />
           </div>
         </main>
         <aside className="basis-1/4 w-1/2 xl:w-80 bg-slate-50 p-6 rounded-xl">
           <div className="mb-6 w-full">
             <CalendarTool eventdates={eventdates} />
           </div>
-          <div className="mb-6">
-            <UpcommingEvents events={upcomingEvents} />
-          </div>
-          <div className="p-4 bg-gray-100 rounded-md">
-            <h2 className="text-lg font-bold">{SUMMARY.CHOOSE_DATE_TITLE}</h2>
-            <p className="text-muted-foreground">
-              {SUMMARY.CHOOSE_DATE_DESCRIPTION}
-            </p>
-            <Button variant="default" className="mt-4">
-              {SUMMARY.VIEW_TIPS_BUTTON}
-            </Button>
+          <div className="mt-6">
+            <SelectedEvent
+              givenEvents={events}
+              group_id={group_id}
+              admin={admin}
+            />
           </div>
         </aside>
       </div>
