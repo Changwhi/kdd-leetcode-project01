b3e7eff36c78153f66e90ca37e18e3e2e221ae06
Soohyeun <soohyeunp@gmail.com>
2024-09-21 20:43:25 -0700

    Redirect to right page for the endpoint only with groupid

--- app/dashboard/[groupId]/page.tsx
+++ app/dashboard/[groupId]/page.tsx
@@ -1,12 +1,36 @@
 "use client";
-import { useEffect } from "react";
-import { useRouter } from 'next/navigation'
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { getIsAdmin } from "@/lib/actions/usergroup";
+import DotLoader from "react-spinners/DotLoader";
 
-export default function Page({params}: {params: {groupId: string}}) {
-    const route = useRouter()
-    useEffect(() => {
-        route.push(`/dashboard/${params.groupId}/user/summary`)
-    })
-  return <div>404 page </div>;
+export default function Page({ params }: { params: { groupId: string } }) {
+  const route = useRouter();
+
+  useEffect(() => {
+    const getUserInfo = async () => {
+      const userInfo = await getIsAdmin(Number(params.groupId));
+
+      if (userInfo.length <= 0) {
+        route.push(`/group`);
+      } else {
+        if (userInfo[0].user_type === 0) {
+          route.push(`/dashboard/${params.groupId}/admin/summary`);
+        } else {
+          route.push(`/dashboard/${params.groupId}/user/summary`);
+        }
+      }
+    };
+    getUserInfo();
+  }, []);
+  return (
+    <div className="flex flex-col  justify-center items-center min-h-screen">
+      <div>
+        <DotLoader color="#6a6868" size={60} />
+      </div>
+      <div className="mt-4">
+        Redirecting...
+      </div>
+    </div>
+  );
 }
-
