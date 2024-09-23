1056343a6377cc541b109f1b694bc62f55114522
Changwhi <whinada@gmail.com>
2024-09-22 23:38:50 -0700

    design: refactor design to remove unnecessary empty space on the right side

--- components/admin/members/index.tsx
+++ components/admin/members/index.tsx
@@ -1,14 +1,14 @@
 import { retrieveAllUsers } from "@/lib/actions/summary";
 import UserTable from "./user-table";
 
-async function Members({group_id}: {group_id: number}) {
+async function Members({ group_id }: { group_id: number }) {
   const users = await retrieveAllUsers({ group_id });
-  
+
   return (
-    <div className="w-full flex flex-col min-h-full lg:flex-row">
-      <main className="basis-3/4 p-6 bg-white lg:pr-6 lg:w-3/4">
+    <div className="w-full flex flex-col min-h-full ">
+      <main className="bg-white">
         <h2 className="text-2xl font-bold pb-10">Members</h2>
-        <UserTable usersInGroup={users}/>
+        <UserTable usersInGroup={users} />
       </main>
     </div>
   );
