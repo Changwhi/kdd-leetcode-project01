121318e0a9253339a4a3cf6cca6e523505b2589c
Changwhi <whinada@gmail.com>
2024-09-22 23:45:28 -0700

    design: remove header for improved UI consistency across pages

--- components/admin/members/index.tsx
+++ components/admin/members/index.tsx
@@ -1,5 +1,6 @@
 import { retrieveAllUsers } from "@/lib/actions/summary";
 import UserTable from "./user-table";
+import { MEMBERS } from "@/text/members";
 
 async function Members({ group_id }: { group_id: number }) {
   const users = await retrieveAllUsers({ group_id });
@@ -7,7 +8,7 @@ async function Members({ group_id }: { group_id: number }) {
   return (
     <div className="w-full flex flex-col min-h-full ">
       <main className="bg-white">
-        <h2 className="text-2xl font-bold pb-10">Members</h2>
+        <h2 className="text-2xl font-bold pb-10">{MEMBERS.MEMBERS_DESCRIPTION}</h2>
         <UserTable usersInGroup={users} />
       </main>
     </div>
