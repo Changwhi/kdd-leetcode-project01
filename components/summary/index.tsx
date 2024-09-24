4cfd3d1263f5ff69532b06346cefab852dd022d7
Changwhi <whinada@gmail.com>
2024-09-23 18:38:59 -0700

    style: adjust spacing for improved UI clarity and readability

--- components/summary/index.tsx
+++ components/summary/index.tsx
@@ -28,25 +28,18 @@ export const Summary = async ({
     <EventProvider>
       <div className="flex flex-col min-h-full lg:flex-row">
         <main className="basis-3/4 p-6 bg-white lg:pr-6 lg:w-3/4">
-          <div className="mb-6">
-            <UpcommingEvents events={upcomingEvents} />
-          </div>
+          <UpcommingEvents events={upcomingEvents} />
           <div className="overflow-x-auto mt-5">
-            <h1 className="text-lg font-bold">{SUMMARY.TABLE_TITLE}</h1>
             <SummaryTable usersInGroup={users} />
           </div>
         </main>
         <aside className="basis-1/4 w-1/2 xl:w-80 bg-slate-50 p-6 rounded-xl">
-          <div className="mb-6 w-full">
-            <CalendarTool eventdates={eventdates} />
-          </div>
-          <div className="mt-6">
-            <SelectedEvent
-              givenEvents={events}
-              group_id={group_id}
-              admin={admin}
-            />
-          </div>
+          <CalendarTool eventdates={eventdates} />
+          <SelectedEvent
+            givenEvents={events}
+            group_id={group_id}
+            admin={admin}
+          />
         </aside>
       </div>
     </EventProvider>
