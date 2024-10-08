ab43e9a6c893d676ec1525e46526664cb24a780a
Soohyeun <soohyeunp@gmail.com>
2024-10-08 00:31:37 -0700

    Fix bug that library was imporeted multiple times

--- components/admin/events/event-card.tsx
+++ components/admin/events/event-card.tsx
@@ -21,7 +21,6 @@ import {
 } from "@/components/ui/dialog"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
-import { Textarea } from "@/components/ui/textarea"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import {
   ExternalLinkIcon,
