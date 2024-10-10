8aa3b7270415225c32e775adc9693062d82d1468
Soohyeun <soohyeunp@gmail.com>
2024-10-09 22:17:17 -0700

    Add setting navbar

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,27 +1,29 @@
-"use server";
+"use client";
 
-import { redirect } from "next/navigation";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { Settings } from "@/components/settings";
 import { SETTINGS_CONSTANTS } from "@/text/settings";
-import { getLoggedInUser } from "@/lib/actions/user";
+import { useState } from "react";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import SettingNavBar from "@/components/settings/settings-nav";
 
 /**
  * This component renders a page for updating user's information
  * @param {params} - An object containing a groupId property, which is the id of the group to fetch events and attendance for.
  * @returns A JSX element representing the page.
  */
-export default async function SettingPage() {
-  const currentUser = await getLoggedInUser();
-  if (!currentUser) {
-    redirect("/api/auth/login"); // Redirect to login if user is not logged in
-    return null; // Important to return null after redirect
-  }
+export default function SettingPage() {
+  const { user } = useUser();
+  const [activeTab, setActiveTab] = useState("profile");
+  const tabs = [
+    { name: "Profile", key: "profile" },
+    { name: "Groups", key: "Groups" },
+  ];
 
   return (
     <div className="flex flex-col min-h-[100dvh]">
-      <Header user={currentUser} />
+      <Header user={user} />
       <main className="flex-1">
         <section className="w-full py-12 md:py-24 lg:py-20 border-y">
           <div className="px-4 md:px-6 space-y-10 xl:space-y-16">
@@ -32,7 +34,10 @@ export default async function SettingPage() {
               <p className="text-muted-foreground mb-8">
                 {SETTINGS_CONSTANTS.SETTINGS_SUB_TITLE}
               </p>
-              <Settings />
+              <div className="flex flex-row space-x-24">
+                <SettingNavBar activeTab={activeTab} setActiveTab={setActiveTab} tabs={tabs}/>
+                {activeTab == "profile" && <Settings />}
+              </div>
             </div>
           </div>
         </section>
