f9c2b5a9f7babf3afd9afd1365062bba8c666924
Soohyeun <soohyeunp@gmail.com>
2024-10-10 00:07:47 -0700

    Fetch data in management group page

--- components/account/userGroupList.tsx
+++ components/account/userGroupList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useToast } from "@/components/ui/use-toast";
 import { Button } from "@/components/ui/button";
 import {
@@ -12,6 +12,7 @@ import {
 } from "@/components/ui/card";
 import { Trash2, LogOut } from "lucide-react";
 import { SETTINGS_CONSTANTS } from "@/text/settings";
+import { getMyGroups } from "@/lib/actions/group";
 
 interface UserGroupListProps {
   email: string;
@@ -19,6 +20,18 @@ interface UserGroupListProps {
 
 export const UserGroupList: React.FC<UserGroupListProps> = ({ email }) => {
   const { toast } = useToast();
+  const [adminGroups, setAdminGroups] = useState([]);
+  const [memberGroups, SetMemberGroups] = useState([]);
+
+  useEffect(() => {
+    const fetchUserGroups = async () => {
+      const myGroups = await getMyGroups({ email });
+      setAdminGroups(myGroups.filter((group) => group.user_type === 0));
+      SetMemberGroups(myGroups.filter((group) => group.user_type === 1));
+    };
+
+    fetchUserGroups();
+  }, []);
 
   const handleQuitGroup = (groupId: number) => {
     // Implement account deletion logic
@@ -30,45 +43,19 @@ export const UserGroupList: React.FC<UserGroupListProps> = ({ email }) => {
     console.log(`Delete group with id: ${groupId}`);
   };
 
-  const adminGroups = [
-    {
-      id: 1,
-      name: "Admin Web Development",
-      description:
-        "A group for discussing web development technologies and best practices.",
-    },
-    {
-      id: 2,
-      name: "Data Science",
-      description:
-        "Explore data analysis, machine learning, and statistical modeling techniques.",
-    }
-  ];
-
-  const memberGroups = [
-    {
-      id: 1,
-      name: "Member Web Development",
-      description:
-        "A group for discussing web development technologies and best practices.",
-    },
-    {
-      id: 2,
-      name: "Data Science",
-      description:
-        "Explore data analysis, machine learning, and statistical modeling techniques.",
-    }
-  ];
-
   return (
     <Card className="w-full max-w-2xl">
       <CardHeader>
         <CardTitle>{SETTINGS_CONSTANTS.MANAGEMENT_GROUPS}</CardTitle>
-        <CardDescription>{SETTINGS_CONSTANTS.MANAGEMENT_GROUPS_DESCRIPTION}</CardDescription>
+        <CardDescription>
+          {SETTINGS_CONSTANTS.MANAGEMENT_GROUPS_DESCRIPTION}
+        </CardDescription>
       </CardHeader>
       <CardContent>
-      <div className="container mx-auto p-4 max-w-2xl">
-          <h1 className="text-xl font-bold mb-6">{SETTINGS_CONSTANTS.GROUP_ADMIN}</h1>
+        <div className="container mx-auto p-4 max-w-2xl">
+          <h1 className="text-xl font-bold mb-6">
+            {SETTINGS_CONSTANTS.GROUP_ADMIN}
+          </h1>
           <div className="space-y-4">
             {adminGroups.map((group) => (
               <Card key={group.id} className="flex items-center p-4">
@@ -87,7 +74,7 @@ export const UserGroupList: React.FC<UserGroupListProps> = ({ email }) => {
                       onClick={() => handleDeleteGroup(group.id)}
                       aria-label={`Quit ${group.name} group`}
                     >
-                      <Trash2 className="h-4 w-4"  />
+                      <Trash2 className="h-4 w-4" />
                     </Button>
                   </div>
                 </CardContent>
@@ -96,7 +83,9 @@ export const UserGroupList: React.FC<UserGroupListProps> = ({ email }) => {
           </div>
         </div>
         <div className="container mx-auto p-4 max-w-2xl">
-          <h1 className="text-xl font-bold mb-6">{SETTINGS_CONSTANTS.GROUP_MEMBER}</h1>
+          <h1 className="text-xl font-bold mb-6">
+            {SETTINGS_CONSTANTS.GROUP_MEMBER}
+          </h1>
           <div className="space-y-4">
             {memberGroups.map((group) => (
               <Card key={group.id} className="flex items-center p-4">
@@ -115,7 +104,7 @@ export const UserGroupList: React.FC<UserGroupListProps> = ({ email }) => {
                       onClick={() => handleQuitGroup(group.id)}
                       aria-label={`Quit ${group.name} group`}
                     >
-                      <LogOut className="h-4 w-4"  />
+                      <LogOut className="h-4 w-4" />
                     </Button>
                   </div>
                 </CardContent>
