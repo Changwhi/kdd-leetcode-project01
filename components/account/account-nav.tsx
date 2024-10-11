385826c8f02b1ab9866455f6fd73e41b6c78b03f
Changwhi <whinada@gmail.com>
2024-10-11 00:08:41 -0700

    fix: add type definitions for AccountNavBar props

--- components/account/account-nav.tsx
+++ components/account/account-nav.tsx
@@ -1,5 +1,21 @@
 "use client";
-export default function AccountNavBar({ activeTab, setActiveTab, tabs }) {
+
+interface Tab {
+  name: string;
+  key: string;
+}
+
+interface AccountNavBarProps {
+  activeTab: string;
+  setActiveTab: (key: string) => void;
+  tabs: Tab[];
+}
+
+export default function AccountNavBar({
+  activeTab,
+  setActiveTab,
+  tabs,
+}: AccountNavBarProps) {
   return (
     <div className="w-28 p-4 border-r">
       <ul className="space-y-4">
@@ -7,7 +23,9 @@ export default function AccountNavBar({ activeTab, setActiveTab, tabs }) {
           <li key={tab.key}>
             <button
               onClick={() => setActiveTab(tab.key)}
-              className={`block py-2 px-4 hover:text-black relative ${activeTab === tab.key ? "font-bold text-black" : "text-gray-800"}`}
+              className={`block py-2 px-4 hover:text-black relative ${
+                activeTab === tab.key ? "font-bold text-black" : "text-gray-800"
+              }`}
             >
               {activeTab === tab.key && (
                 <span className="absolute left-0 top-0 h-full w-1 bg-black"></span>
